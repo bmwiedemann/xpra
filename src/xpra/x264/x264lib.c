bmwiedemann/xpra

commit ab9fde38696e8895f7168fcc3eccbe50d3b3cbea

move free() calls to the same file where we do malloc(), and use xmemalign for allocating csc image output buffers!
git-svn-id: https://xpra.org/svn/Xpra/trunk@1530 3bb7dfac-3a0b-4e04-842a-767bc560f471

src/xpra/x264/x264lib.c
@@ -207,6 +207,7 @@ void clean_encoder(struct x264lib_ctx *ctx)
 		x264_encoder_close(ctx->encoder);
 		ctx->encoder = NULL;
 	}
+	free(self.context);
 }
 
 #else
@@ -270,6 +271,7 @@ void clean_decoder(struct x264lib_ctx *ctx)
 		sws_freeContext(ctx->yuv2rgb);
 		ctx->yuv2rgb = NULL;
 	}
+	free(ctx);
 }
 
 #ifndef _WIN32
@@ -349,7 +351,7 @@ int csc_image_yuv2rgb(struct x264lib_ctx *ctx, uint8_t *in[3], const int stride[
 	if (!ctx->yuv2rgb)
 		return 1;
 
-	avpicture_fill(&pic, malloc(ctx->height * ctx->width * 3), PIX_FMT_RGB24, ctx->width, ctx->height);
+	avpicture_fill(&pic, xmemalign(ctx->height * ctx->width * 3), PIX_FMT_RGB24, ctx->width, ctx->height);
 
 	sws_scale(ctx->yuv2rgb, (const uint8_t * const*) in, stride, 0, ctx->height, pic.data, pic.linesize);
 

src/xpra/x264/codec.pyx
@@ -78,7 +78,6 @@ cdef class Decoder(xcoder):
     def clean(self):
         if self.context!=NULL:
             clean_decoder(self.context)
-            free(self.context)
             self.context = NULL
 
     def decompress_image_to_yuv(self, input, options):
@@ -157,7 +156,6 @@ cdef class Encoder(xcoder):
     def clean(self):                        #@DuplicatedSignature
         if self.context!=NULL:
             clean_encoder(self.context)
-            free(self.context)
             self.context = NULL
 
     def get_client_options(self, options):

commit 11b944ae43fe09dd028a05ed36bdca8694d38fd9

fix bug introduced by r1530: set_decoder_csc_format cannot free the whole context, we want to re-use the same structure but re-initialize it, so split the "free()" part from the "clean" part.
git-svn-id: https://xpra.org/svn/Xpra/trunk@1532 3bb7dfac-3a0b-4e04-842a-767bc560f471

src/xpra/x264/x264lib.c
@@ -262,6 +262,11 @@ struct x264lib_ctx *init_decoder(int width, int height, int csc_fmt)
 
 void clean_decoder(struct x264lib_ctx *ctx)
 {
+	do_clean_decoder(ctx);
+	free(ctx);
+}
+void do_clean_decoder(struct x264lib_ctx *ctx)
+{
 	if (ctx->codec_ctx) {
 		avcodec_close(ctx->codec_ctx);
 		av_free(ctx->codec_ctx);
@@ -271,7 +276,6 @@ void clean_decoder(struct x264lib_ctx *ctx)
 		sws_freeContext(ctx->yuv2rgb);
 		ctx->yuv2rgb = NULL;
 	}
-	free(ctx);
 }
 
 #ifndef _WIN32
@@ -369,7 +373,7 @@ void set_decoder_csc_format(struct x264lib_ctx *ctx, int csc_fmt)
 		csc_fmt = PIX_FMT_YUV420P;
 	if (ctx->csc_format!=csc_fmt) {
 		//we need to re-initialize with the new format:
-		clean_decoder(ctx);
+		do_clean_decoder(ctx);
 		if (init_decoder_context(ctx, ctx->width, ctx->height, csc_fmt)) {
 			fprintf(stderr, "Failed to reconfigure decoder\n");
 		}