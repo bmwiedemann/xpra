bmwiedemann/xpra

commit 4adfdba990e82f16b928f99b8460577bc0c3f7df

sort out the mess that has become "xpra info" by using a more consistent namespace throughout
git-svn-id: https://xpra.org/svn/Xpra/trunk@3680 3bb7dfac-3a0b-4e04-842a-767bc560f471

src/xpra/client/gtk_base/session_info.py
@@ -579,14 +579,15 @@ class SessionInfo(gtk.Window):
             setlabels(self.client_latency_labels, cpl)
         if self.client.windows_enabled:
             if self.client.server_info_request:
-                def values_from_info(prefix):
-                    def getv(suffix):
+                def values_from_info(prefix, alt=None):
+                    def getv(suffix, alt=None):
                         if self.client.server_last_info is None:
                             return ""
-                        return self.client.server_last_info.get(prefix+"."+suffix, "")
+                        altv = self.client.server_last_info.get(alt+"."+suffix, "")
+                        return self.client.server_last_info.get(prefix+"."+suffix, "", altv)
                     return getv("cur"), getv("min"), getv("avg"), getv("90p"), getv("max")
-                setall(self.batch_labels, values_from_info("batch_delay"))
-                setall(self.damage_labels, values_from_info("damage_out_latency"))
+                setall(self.batch_labels, values_from_info("batch_delay", "batch.delay"))
+                setall(self.damage_labels, values_from_info("damage_out_latency", "damage.out_latency"))
                 def all_values_from_info(window_prop):
                     def avg(values):
                         if not values:

src/tests/xpra/test_measure_perf.py
@@ -67,7 +67,7 @@ IPTABLES_CMD = ["sudo", "/usr/sbin/iptables"]
 TRICKLE_BIN = "/usr/bin/trickle"
 TCBENCH = "/opt/VirtualGL/bin/tcbench"
 TCBENCH_LOG = "./tcbench.log"
-XORG_BIN = "/usr/local/bin/Xorg"
+XORG_BIN = "/usr/bin/Xorg"
 VGLRUN_BIN = "/usr/bin/vglrun"
 
 #the glx tests:
@@ -649,30 +649,30 @@ def xpra_get_stats(last_record=None):
                 return v
         return default_value
     return [
-            get(["regions_per_second"]),
-            get(["pixels_per_second"]),
-            get(["pixels_encoded_per_second"]),
-            get(["pixels_decoded_per_second"]),
-            get(["batch_delay.min", "min_batch_delay"]),
-            get(["batch_delay.max", "max_batch_delay"]),
-            get(["batch_delay.avg", "avg_batch_delay"]),
-            (int(get(["input_packetcount"], 0))-last_input_packetcount)/MEASURE_TIME,
-            (int(get(["input_bytecount"], 0))-last_input_bytecount)/MEASURE_TIME,
-            (int(get(["output_packetcount"], 0))-last_output_packetcount)/MEASURE_TIME,
-            (int(get(["output_bytecount"], 0))-last_output_bytecount)/MEASURE_TIME,
-            (int(get(["output_mmap_bytecount"], 0))-last_mmap_bytes)/MEASURE_TIME,
-            get(["client_latency.min", "min_client_latency"]),
-            get(["client_latency.max", "max_client_latency"]),
-            get(["client_latency.avg", "avg_client_latency"]),
-            get(["client_ping_latency.min"]),
-            get(["client_ping_latency.max"]),
-            get(["client_ping_latency.avg"]),
-            get(["server_ping_latency.min", "server_latency.min", "min_server_latency"]),
-            get(["server_ping_latency.max", "server_latency.max", "max_server_latency"]),
-            get(["server_ping_latency.avg", "server_latency.avg", "avg_server_latency"]),
-            get(["damage_in_latency.min"]),
-            get(["damage_in_latency.max"]),
-            get(["damage_in_latency.avg"]),
+            get(["encoding.regions_per_second", "regions_per_second"]),
+            get(["encoding.pixels_per_second", "pixels_per_second"]),
+            get(["encoding.pixels_encoded_per_second", "pixels_encoded_per_second"]),
+            get(["encoding.pixels_decoded_per_second", "pixels_decoded_per_second"]),
+            get(["batch.delay.min", "batch_delay.min", "min_batch_delay"]),
+            get(["batch.delay.max", "batch_delay.max", "max_batch_delay"]),
+            get(["batch.delay.avg", "batch_delay.avg", "avg_batch_delay"]),
+            (int(get(["net.input.packetcount", "input_packetcount"], 0))-last_input_packetcount)/MEASURE_TIME,
+            (int(get(["net.input.bytecount", "input_bytecount"], 0))-last_input_bytecount)/MEASURE_TIME,
+            (int(get(["net.output.packetcount", "output_packetcount"], 0))-last_output_packetcount)/MEASURE_TIME,
+            (int(get(["net.output.bytecount", "output_bytecount"], 0))-last_output_bytecount)/MEASURE_TIME,
+            (int(get(["net.output.mmap_bytecount", "output_mmap_bytecount"], 0))-last_mmap_bytes)/MEASURE_TIME,
+            get(["client.latency.min", "client_latency.min", "min_client_latency"]),
+            get(["client.latency.max", "client_latency.max", "max_client_latency"]),
+            get(["client.latency.avg", "client_latency.avg", "avg_client_latency"]),
+            get(["client.ping_latency.min", "client_ping_latency.min"]),
+            get(["client.ping_latency.max", "client_ping_latency.max"]),
+            get(["client.ping_latency.avg", "client_ping_latency.avg"]),
+            get(["server.ping_latency.min", "server_ping_latency.min", "server_latency.min", "min_server_latency"]),
+            get(["server.ping_latency.max", "server_ping_latency.max", "server_latency.max", "max_server_latency"]),
+            get(["server.ping_latency.avg", "server_ping_latency.avg", "server_latency.avg", "avg_server_latency"]),
+            get(["damage.in_latency.min", "damage_in_latency.min"]),
+            get(["damage.in_latency.max", "damage_in_latency.max"]),
+            get(["damage.in_latency.avg", "damage_in_latency.avg"]),
            ]
 
 def get_xpra_start_server_command():

src/xpra/clipboard/clipboard_base.py
@@ -64,6 +64,9 @@ class ClipboardProtocolHelperBase(object):
         self.init_packet_handlers()
         self.init_proxies(clipboards)
 
+    def __str__(self):
+        return "ClipboardProtocolHelperBase"
+
     def cleanup(self):
         def nosend(*args):
             pass

src/xpra/clipboard/gdk_clipboard.py
@@ -22,6 +22,9 @@ class GDKClipboardProtocolHelper(ClipboardProtocolHelperBase):
     def __init__(self, send_packet_cb, progress_cb=None, clipboards=CLIPBOARDS, filter_res=None):
         ClipboardProtocolHelperBase.__init__(self, send_packet_cb, progress_cb, clipboards, filter_res)
 
+    def __str__(self):
+        return "GDKClipboardProtocolHelper"
+
     def _do_munge_raw_selection_to_wire(self, target, datatype, dataformat, data):
         if dataformat == 32 and datatype in ("ATOM", "ATOM_PAIR"):
             debug("_do_munge_raw_selection_to_wire(%s, %s, %s, %s:%s:%s) using gdk atom code", target, datatype, dataformat, type(data), len(data), list(data))

src/xpra/net/protocol.py
@@ -198,12 +198,12 @@ class Protocol(object):
         return  [x for x in [self._write_thread, self._read_thread, self._read_parser_thread] if x is not None]
 
     def add_stats(self, info,  suffix=""):
-        info["input_bytecount%s" % suffix] = self._conn.input_bytecount
-        info["input_packetcount%s" % suffix] = self.input_packetcount
-        info["input_raw_packetcount%s" % suffix] = self.input_raw_packetcount
-        info["output_bytecount%s" % suffix] = self._conn.output_bytecount
-        info["output_packetcount%s" % suffix] = self.output_packetcount
-        info["output_raw_packetcount%s" % suffix] = self.output_raw_packetcount
+        info["net.input.bytecount%s" % suffix] = self._conn.input_bytecount
+        info["net.input.packetcount%s" % suffix] = self.input_packetcount
+        info["net.input.raw_packetcount%s" % suffix] = self.input_raw_packetcount
+        info["net.output.bytecount%s" % suffix] = self._conn.output_bytecount
+        info["net.output.packetcount%s" % suffix] = self.output_packetcount
+        info["net.output.raw_packetcount%s" % suffix] = self.output_raw_packetcount
 
     def start(self):
         def do_start():

src/xpra/platform/darwin/osx_clipboard.py
@@ -20,6 +20,9 @@ class OSXClipboardProtocolHelper(GDKClipboardProtocolHelper):
     def make_proxy(self, clipboard):
         return OSXClipboardProxy(clipboard)
 
+    def __str__(self):
+        return "OSXClipboardProtocolHelper"
+
 
 class OSXClipboardProxy(ClipboardProxy):
 

src/xpra/platform/darwin/shadow_server.py
@@ -132,6 +132,6 @@ class ShadowServer(ShadowServerBase, GTKServerBase):
 
     def get_info(self, proto):
         info = GTKServerBase.get_info(self, proto)
-        info["shadow"] = True
-        info["server_type"] = "Python/gtk2/osx-shadow"
+        info["features.shadow"] = True
+        info["server.type"] = "Python/gtk2/osx-shadow"
         return info

src/xpra/platform/win32/shadow_server.py
@@ -71,6 +71,6 @@ class ShadowServer(ShadowServerBase, GTKServerBase):
 
     def get_info(self, proto):
         info = GTKServerBase.get_info(self, proto)
-        info["shadow"] = True
-        info["server_type"] = "Python/gtk2/win32-shadow"
+        info["features.shadow"] = True
+        info["server.type"] = "Python/gtk2/win32-shadow"
         return info

commit 0c4a7dd65e0117a27c35bdfaa7ac01bb852f8452

fix broken syntax introduced in r3680
git-svn-id: https://xpra.org/svn/Xpra/trunk@3687 3bb7dfac-3a0b-4e04-842a-767bc560f471

src/xpra/client/gtk_base/session_info.py
@@ -580,11 +580,13 @@ class SessionInfo(gtk.Window):
         if self.client.windows_enabled:
             if self.client.server_info_request:
                 def values_from_info(prefix, alt=None):
-                    def getv(suffix, alt=None):
+                    def getv(suffix):
                         if self.client.server_last_info is None:
                             return ""
-                        altv = self.client.server_last_info.get(alt+"."+suffix, "")
-                        return self.client.server_last_info.get(prefix+"."+suffix, "", altv)
+                        altv = ""
+                        if alt:
+                            altv = self.client.server_last_info.get(alt+"."+suffix, "")
+                        return self.client.server_last_info.get(prefix+"."+suffix, altv)
                     return getv("cur"), getv("min"), getv("avg"), getv("90p"), getv("max")
                 setall(self.batch_labels, values_from_info("batch_delay", "batch.delay"))
                 setall(self.damage_labels, values_from_info("damage_out_latency", "damage.out_latency"))